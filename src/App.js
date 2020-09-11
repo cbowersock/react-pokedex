d15226a7ab86ff0f1be52cdfeb4e3e2531fadcdf
Clint Bowersock <52680761+cbowersock@users.noreply.github.com>
2020-09-10 22:02:20 -0400

    MVP complete

--- src/App.js
+++ src/App.js
@@ -1,9 +1,23 @@
 import React from 'react';
+import ApolloClient from 'apollo-boost';
+import {ApolloProvider} from '@apollo/react-hooks';
+import PokemonContainer from './containers/PokemonContainer';
+
+import './index.css';
 
 const App = () => {
+
+    const client = new ApolloClient({
+        uri: 'https://graphql-pokemon2.vercel.app/'
+    });
+
     return (
         <>
-        <h1>Pokedex</h1>
+        <ApolloProvider client={client}>
+            <main>
+                <PokemonContainer/>
+            </main>
+        </ApolloProvider>
         </>
     )
 }
